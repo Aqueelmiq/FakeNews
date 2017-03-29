d2a54a4e495d607df7a191c59f8a5a5fe62dc912
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-03-29 00:52:08 -0500

    updated

--- fakenews.js
+++ fakenews.js
@@ -1,23 +1,10 @@
-//Just some chrome extension that removes contents from faceboook
-$('th:contains("Died")').parent().hide()
-
-$('td:contains("death")').parent().hide()
-
-$('span:contains("Death")').parent().hide()
-
-$('p').not('.infobox p').first().html(function() {
-	return $(this).html().replace('was', 'is')
-})
-
-var hidden = ['died','remains','funeral','death','autopsy','suicide','cremated','interred','mausoleum']
-hidden.forEach(function(word) {
-	$('p:contains('+word+')').hide()
-	$('li:contains('+word+')').hide()
-	$('span:contains('+word+')').hide()
-})
-
-$('p').each(function() {
-    var html = $(this).html()
-    $(this).html(html.replace('was survived by', 'lives with'))
-    $(this).html(html.replace(/(&nbsp;| )– [0-9a-zA-Z, ]+\)/, ')'))
-})
+$('*').each(function(){
+		console.log('Hi');
+    if ($(this).is('img')) {
+			$(this).css({
+        	"-webkit-transform": "rotate(90deg)",
+        	"-moz-transform": "rotate(90deg)",
+        	"transform": "rotate(90deg)"
+    	});
+    }
+});
