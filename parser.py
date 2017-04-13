76a0bd5a20e112ce729f46e02412e8ab626bec79
Jessie Nelson <jnelso23@hawk.iit.edu>
2017-04-13 12:34:55 -0500

    Fixing compare_known_sources() to reflect master list of source biases'

--- parser.py
+++ parser.py
@@ -1,47 +1,69 @@
 import ratings
-import re
 
 class parser:
 	def __init__(self, url):
-		self.trusted_sources = ['cnn.com', 'bbc.co.uk']
+		#Global variables
 		self.address = url
-		self.leanings = []
+		self.list = 'leanings.txt'
+		self.trusted = ['bbc',
+						'npr',
+						'pbs',
+						'wsj',
+						'abc',
+						'cbs',
+						'nbc',
+						'cnn',
+						'usatoday']
+						
+						
 	
 	def parse(self):
 		#Sends address to recommended and not recommended methods. 
 		#Returns a recommendation
 		r = self.recommended()
 		nr = self.not_recommended()
-		r_score = r - nr
-		if r_score > 2:
-			return Ratings.RECOMMENDED
-		elif r_score < -2:
-			return Ratings.NOT_RECOMMENDED
+		r_score = r - nr + self.compare_known_sources()
+		if r_score > 1:
+			return ratings.Ratings.RECOMMENDED
+		elif r_score < -1:
+			return ratings.Ratings.NOT_RECOMMENDED
 		else:
-			return Ratings.UNCERTAIN
-		
+			return ratings.Ratings.UNCERTAIN
+	
 	def not_recommended(self):
+		# Assigns a not_recommended score to a url
 		score = 0
 		# Check for known urls
 		if 'com.co' in self.address:
 			score -=2
+		return score
 	
 	def recommended(self):
+		# Assigns a recommended score to a url
 		score = 0
 		# Check for known urls
-		score += 1
+		for s in self.trusted:
+			if s in self.address:
+				score += 1
+		return score
 	
-	def build_leanings_index(self, path):
-		filename = path
-		lines = open(filename).read()
-		tokens = re.split('\t' ,lines.lower());
-		entry = []
-		iter = 0
-		for t in tokens:
-			entry.append(t)
-			self.leanings.append(entry)
-			entry = []
-		
-			
-		
-		
+	def build_leanings_index(self):
+		# Returns leanings of known sources
+		leanings = []
+		filename = self.list
+		with open(filename) as inputfile:
+			for line in inputfile:
+				leanings.append(line.strip().split('\t'))
+		return leanings
+	
+	def compare_known_sources(self):
+		# Returns a -1, 0, or 1 depending on source leanings
+		index = self.build_leanings_index()
+		for i in index:
+			print i
+			if self.address in i[3]:
+				if i[1] is 'Center':
+					return 1
+				else:
+					return -1
+		return 0
